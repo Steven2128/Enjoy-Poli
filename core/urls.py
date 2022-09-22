65cb744c478100ba5b2e399becb1d1c260792ba2
Stephen Alarcón <slj212885@gmail.com>
2022-09-21 23:15:42 -0500

    Build: Estructura inicial del proyecto

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+# Django
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
+]
