651375517bc62bd86239035fc67e5b5580a55f72
Richard Stephen Barboza Alarcón <rbarboza@tredasolutions.com>
2022-09-27 13:28:10 -0500

    feat(develop): Agrega urls para logout

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 # Django
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.views import LogoutView
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+    # Logout
+    path('logout/', LogoutView.as_view(), name='logout'),
+    # Home
     path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
+    # Programs
+    path('programs/', include('wellness.urls'))
 ]
