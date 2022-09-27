a53cef2e566edf35d6d58d303caaa36b2d8afa83
Richard Stephen Barboza Alarcón <rbarboza@tredasolutions.com>
2022-09-27 13:27:22 -0500

    feat(develop): Agrega la app users

---
+++ users/admin.py
@@ -0,0 +1,7 @@
+# Django
+from django.contrib import admin
+# Models
+from .models import *
+
+admin.site.register(Teacher)
+admin.site.register(Student)
---
+++ users/models.py
@@ -0,0 +1,31 @@
+# Django
+from django.contrib.auth.models import User
+from django.db import models
+# Models wellness
+from wellness.models import *
+
+
+class Teacher(models.Model):
+    """Model Teacher"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=100, blank=False, null=False, verbose_name='Especialización del docente')
+
+    def __str__(self):
+        return "{} - {}".format(self.user.get_full_name(), self.specialization)
+
+    class Meta:
+        verbose_name = 'Docente'
+        verbose_name_plural = 'Docentes'
+
+
+class Student(models.Model):
+    """Model Student"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    career = models.ForeignKey(Career, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+    class Meta:
+        verbose_name = 'Estudiante'
+        verbose_name_plural = 'Estudiantes'
