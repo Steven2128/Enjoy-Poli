1ed695feae95a9e5741008b8eff4fbcae5733ab2
Richard Stephen Barboza Alarcón <rbarboza@tredasolutions.com>
2022-10-21 16:24:45 -0500

    feat(core): Login

--- core/settings.py
+++ core/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
@@ -138,3 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login'
--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,19 @@
 # Django
 from django.contrib import admin
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
+    # Login
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     # Logout
     path('logout/', LogoutView.as_view(), name='logout'),
     # Home
-    path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
+    path('', login_required(TemplateView.as_view(template_name='home/index.html')), name='index'),
     # Programs
     path('bienestar/', include('wellness.urls'))
 ]
